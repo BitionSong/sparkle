69fd11705ee9e49d070271ad289d04eaf4a62762
xishang0128 <xishang02@gmail.com>
2025-08-29 22:52:24 +0800

    correct yaml preprocessing

--- src/main/utils/yaml.ts
+++ src/main/utils/yaml.ts
@@ -2,7 +2,7 @@ import yaml from 'yaml'
 
 export function parseYaml<T = unknown>(content: string): T {
   const processedContent = content.replace(
-    /^(\s*short-id:\s*)([^"\s\n}{]+)/gm,
+    /^(\s*short-id:\s*)([^"'\s\n}{]+)/gm,
     (match, prefix, value) => {
       const nullValues = ['null', 'Null', 'NULL', '~']
       return nullValues.includes(value) ? match : `${prefix}"${value}"`
